e815399f5857c92abfce217d15fc45793d320b45
oleh <dovgalyuk9090@gmail.com>
2024-10-12 11:41:20 +0200

    add prettier && configure project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
+import { RouterOutlet } from '@angular/router';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  standalone: true,
+  imports: [RouterOutlet],
 })
 export class AppComponent {
   title = 'medium_clone';
